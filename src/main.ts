192893be977d8cc8eba6a3450e7a078778aac298
Landon Cassman <Landon.Cassman@gmail.com>
2022-10-08 13:41:42 -0400

    Trying to change color of winner

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import './style.css'
 
-function updateTeam(id: string) {
-  console.log(id)
+function setupTeam(id: string) {
+  let score = 0
   const inputElement = document.querySelector(
     `.${id} input`
   ) as HTMLInputElement
   const headingElement = document.querySelector(
     `.${id} h2`
   ) as HTMLHeadingElement
+  const addButton = document.querySelector(`.${id} .add`) as HTMLElement
+  const scoreElement = document.querySelector(`.${id} h3`) as HTMLElement
+  const subtractButton = document.querySelector(
+    `.${id} .subtract`
+  ) as HTMLElement
+  const resetButton = document.querySelector('.reset') as HTMLButtonElement
+  const teamSection = document.querySelector(`section .${id}`) as HTMLElement
 
   console.log({ inputElement, headingElement })
+
   function updateTeamName(event: Event) {
     const elementChanged = event.target
     console.log('Update', headingElement)
@@ -19,31 +27,39 @@ function updateTeam(id: string) {
     }
   }
   inputElement.addEventListener('input', updateTeamName)
-}
 
-updateTeam('team1')
-updateTeam('team2')
-
-function changeScore(id: string) {
-  const increaseScoreButton = document.querySelector(
-    `.${id} i`
-  ) as HTMLButtonElement
-  const scoreHeadingElement = document.querySelector(
-    `.${id} h3`
-  ) as HTMLHeadingElement
-
-  console.log({ increaseScoreButton, scoreHeadingElement })
-
-  function increaseScore(event: Event)
-  {
-    const scoreElementChanged = event.target
-    
-    if (scoreElementChanged instanceof HTMLButtonElement) {
-    const currentScore = plusButtonPressed.value
-    scoreHeadingElement.innerText +=
+  function resetScores(_event: Event) {
+    score = 0
+    scoreElement.innerText = score.toString()
   }
-  
-  increaseScore.addEventListener('click', increaseScore(event))
+  resetButton?.addEventListener('click', resetScores)
+
+  function incrementScore(_event: Event) {
+    if (score < 22) {
+      score += 1
+      scoreElement.innerText = score.toString()
+    }
+    if (score === 21) {
+      alert(`${headingElement.innerText} got 21 points and won the game!`)
+      teamSection.style.backgroundColor = 'lime'
+      resetScores(_event)
+    }
+  }
+  addButton?.addEventListener('click', incrementScore)
+  teamSection?.addEventListener('click', incrementScore)
+
+  function decrementScore(_event: Event) {
+    console.log('sub')
+    if (score > 0) {
+      score -= 1
+      scoreElement.innerText = score.toString()
+    }
+  }
+  console.log(subtractButton)
+  subtractButton?.addEventListener('click', decrementScore)
+
+  // function changeWinningTeamColor =
 }
-changeScore('team1')
-changeScore('team2')
+
+setupTeam('team1')
+setupTeam('team2')
